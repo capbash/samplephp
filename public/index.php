de8cd98f8568bdc0e69e73dc407e422bb3cc9d98
Andrew Forward <aforward@gmail.com>
2017-02-13 10:52:53 -0500

    Add a simple HTML router

--- public/index.php
+++ public/index.php
@@ -1,2 +1,8 @@
-V0.2
-<?php phpinfo(); ?>
+<?php
+require_once('_config.php');
+
+$GLOBALS["viewables"]["head_title"] = "Homepage";
+$GLOBALS["viewables"]["route"] = "homepage";
+$GLOBALS["viewables"]["h1"] = "Hello World";
+
+require_view('layout/layout');
