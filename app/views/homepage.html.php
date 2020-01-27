53b3be74567a4093a2ebb7e40761316b83ff5587
Andrew Forward <aforward@gmail.com>
2020-01-27 12:59:41 -0500

    Add a connection to the database

--- app/views/homepage.html.php
+++ app/views/homepage.html.php
@@ -1,5 +1,9 @@
 <h1><?php echo g("h1") ?></h1>
 
+<?php
+  print_r(Db::sql("select * from actions"));
+?>
+
 <form method="POST">
   <input name="myName" placeholder="Your Name">
   <button>Go</button>
